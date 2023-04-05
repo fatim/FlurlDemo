51f2c89aad06bf63a46b051deceb587451e0b8d1
T <T>
2023-04-05 19:45:18 +0100

    DI Factory version

--- FlurtDemo.Client/Program.cs
+++ FlurtDemo.Client/Program.cs
@@ -1,23 +1,33 @@
 ﻿using System.Diagnostics;
-using Flurl;
 using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var apiEndpoint = "http://localhost:5000/square";
+
+var sc = new ServiceCollection();
+sc.AddSingleton<IFlurlClient, FlurlClient>();
+sc.AddSingleton<IFlurlClientFactory, PerBaseUrlFlurlClientFactory>();
+var provider = sc.BuildServiceProvider();
+
+var factory = provider.GetService<IFlurlClientFactory>();
+var client = factory.Get(apiEndpoint);
 
 var sw = new Stopwatch();
 sw.Start();
 
-var task1 = SendRequest(apiEndpoint, 5);
-var task2 = SendRequest(apiEndpoint, 6);
+var task1 = SendRequest(client, 5);
+var task2 = SendRequest(client, 6);
 await Task.WhenAll(task1, task2);
 
 sw.Stop();
 Console.WriteLine($"{sw.ElapsedMilliseconds}ms");
 
-Task<string> SendRequest(string url, int parameter)
+Task<string> SendRequest(IFlurlClient client, int parameter)
 {
-    var task = url
-        .SetQueryParams(new {value = parameter})
+    var task = client
+        .Request()
+        .SetQueryParams(new {value = 3})
         .GetStringAsync();
     return task;
 }
